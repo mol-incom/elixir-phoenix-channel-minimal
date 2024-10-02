e5d3768d2e04f272516acd4b86c09ea514687673
Michael Bisgaard Olesen <mol@in-commodities.com>
2024-10-02 10:29:19 +0200

    Removed controller, router, etc.

--- lib/phoenix_channel_minimal_web/endpoint.ex
+++ lib/phoenix_channel_minimal_web/endpoint.ex
@@ -19,24 +19,6 @@ defmodule PhoenixChannelMinimalWeb.Endpoint do
     websocket: true,
     longpoll: false
 
-  # Serve at "/" the static files from "priv/static" directory.
-  #
-  # You should set gzip to true if you are running phx.digest
-  # when deploying your static files in production.
-  plug Plug.Static,
-    at: "/",
-    from: :phoenix_channel_minimal,
-    gzip: false,
-    only: PhoenixChannelMinimalWeb.static_paths()
-
-  # Code reloading can be explicitly enabled under the
-  # :code_reloader configuration of your endpoint.
-  if code_reloading? do
-    socket "/phoenix/live_reload/socket", Phoenix.LiveReloader.Socket
-    plug Phoenix.LiveReloader
-    plug Phoenix.CodeReloader
-  end
-
   plug Phoenix.LiveDashboard.RequestLogger,
     param_key: "request_logger",
     cookie_key: "request_logger"
@@ -52,5 +34,4 @@ defmodule PhoenixChannelMinimalWeb.Endpoint do
   plug Plug.MethodOverride
   plug Plug.Head
   plug Plug.Session, @session_options
-  plug PhoenixChannelMinimalWeb.Router
 end
