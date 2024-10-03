c3d4efa052fe4219662cb7ed8a79203b83d0e839
Michael Bisgaard Olesen <mol@in-commodities.com>
2024-10-03 10:07:32 +0200

    Remove tests and add lock file

--- test/phoenix_channel_minimal_web/controllers/page_controller_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule PhoenixChannelMinimalWeb.PageControllerTest do
-  use PhoenixChannelMinimalWeb.ConnCase
-
-  test "GET /", %{conn: conn} do
-    conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
-  end
-end
