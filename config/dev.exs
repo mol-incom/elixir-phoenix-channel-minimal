892d59fa7aebad54f6fc6195cd43f94135f9ee34
Michael Bisgaard Olesen <mol@in-commodities.com>
2024-10-03 10:04:54 +0200

    Re-add dev debugging
    This results in HTTP requests (to, say `/`) getting an error page rather than errors in the log.

--- config/dev.exs
+++ config/dev.exs
@@ -1,7 +1,8 @@
 import Config
 
 config :phoenix_channel_minimal, PhoenixChannelMinimalWeb.Endpoint,
-  http: [ip: {127, 0, 0, 1}, port: 4000]
+  http: [ip: {127, 0, 0, 1}, port: 4000],
+  debug_errors: true
 
 # Do not include metadata nor timestamps in development logs
 config :logger, :console, format: "[$level] $message\n"
